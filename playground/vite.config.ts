d78a5865245c6bc79516cba1e06edf38de42d24c
maggot_code <63178315+maggot-code@users.noreply.github.com>
2024-04-13 20:53:44 +0800

    fix: setup openapi generate typescript plugin

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import unpluginOpenapiGenTS from "unplugin-openapi-gen-ts/vite";
+import { definePlugin as createSwagger } from "@kubb/swagger";
+import { definePlugin as createSwaggerTS } from "@kubb/swagger-ts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        unpluginOpenapiGenTS({
+            config: {
+                input: {
+                    path: "./apis/openapi.yaml",
+                },
+                output: {
+                    path: "./src/interface",
+                    clean: true,
+                },
+                plugins: [
+                    createSwagger({ output: false }),
+                    createSwaggerTS({ output: { path: "models" } }),
+                ],
+            },
+        }),
+    ],
 });
