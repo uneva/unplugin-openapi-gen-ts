08063e7292673188e7629eb151f76e28ecbb2013
maggot_code <63178315+maggot-code@users.noreply.github.com>
2024-04-13 20:55:01 +0800

    feat: generate interface models

---
+++ playground/src/interface/models/Pet.ts
@@ -0,0 +1,33 @@
+import { Category } from "./Category";
+import { Tag } from "./Tag";
+
+ export const petStatus = {
+    "available": "available",
+    "pending": "pending",
+    "sold": "sold"
+} as const;
+export type PetStatus = (typeof petStatus)[keyof typeof petStatus];
+export type Pet = {
+    /**
+     * @type integer | undefined int64
+    */
+    id?: number;
+    /**
+     * @type string
+    */
+    name: string;
+    category?: Category;
+    /**
+     * @type array
+    */
+    photoUrls: string[];
+    /**
+     * @type array | undefined
+    */
+    tags?: Tag[];
+    /**
+     * @description pet status in the store
+     * @type string | undefined
+    */
+    status?: PetStatus;
+};
